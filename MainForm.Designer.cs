6a77e8cabbbd0a71cf77839ef2a35b247d10f884
Alexander Iskhakov <icehackoff@gmail.com>
2016-07-31 22:35:47 +0300

    start with main form and authorization

---
+++ MainForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace VkAudiosaver
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browser = new System.Windows.Forms.WebBrowser();
+            this.songListBox = new System.Windows.Forms.ListBox();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // browser
+            // 
+            this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browser.Location = new System.Drawing.Point(0, 0);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(484, 561);
+            this.browser.TabIndex = 0;
+            this.browser.Visible = false;
+            this.browser.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.browser_Navigated);
+            // 
+            // songListBox
+            // 
+            this.songListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.songListBox.FormattingEnabled = true;
+            this.songListBox.Location = new System.Drawing.Point(0, -4);
+            this.songListBox.Name = "songListBox";
+            this.songListBox.Size = new System.Drawing.Size(484, 537);
+            this.songListBox.TabIndex = 1;
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(0, 538);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(484, 23);
+            this.loadButton.TabIndex = 2;
+            this.loadButton.Text = "Загрузить";
+            this.loadButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 561);
+            this.Controls.Add(this.browser);
+            this.Controls.Add(this.songListBox);
+            this.Controls.Add(this.loadButton);
+            this.Name = "MainForm";
+            this.Text = "VK Audiosaver";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser browser;
+        private System.Windows.Forms.ListBox songListBox;
+        private System.Windows.Forms.Button loadButton;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Web;
+using System.Globalization;
+
+namespace VkAudiosaver
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private static class VkData
+        {
+            //id зарегистрированного в вк приложения
+            public const string clientId = "*******";
+            //стандартная страница редиректа
+            public const string redirectUri = "https://oauth.vk.com/blank.html";
+            //битовая маска доступа, соотв. доступу к аудио
+            public const int scope = 8;
+            //ключ доступа к API
+            public static string accessToken;
+            //id залогинившегося пользователя
+            public static string userId;
+        }
+
+        private List<Song> SongList;
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //авторизуемся
+            Authorization();
+        }
+
+        private void browser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            if (e.Url.GetLeftPart(UriPartial.Path) == VkData.redirectUri)
+            {
+                browser.Visible = false;
+                //при перенаправлении после авторизации извлекаем из url ключ доступа и id пользователя
+                System.Collections.Specialized.NameValueCollection queryParams =
+                                                    HttpUtility.ParseQueryString(e.Url.Fragment.Substring(1));
+                VkData.accessToken = queryParams.Get("access_token");
+                VkData.userId = queryParams.Get("user_id");
+
+                //в случае успеха переходим к составлению списка
+                if (queryParams.Get("error") == null) FillSongList();
+                else
+                {
+                    //при ошибке выводим сообщение и снова пробуем авторизоваться
+                    MessageBox.Show(queryParams.Get("error_description").Replace('_', ' '),
+                        new CultureInfo("en-US").TextInfo.ToTitleCase(queryParams.Get("error").Replace('_', ' ')),
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Authorization();
+                }
+            }
+        }
+
+        private void Authorization()
+        {
+            browser.Visible = true;
+            //открытие диалога авторизации
+            browser.Navigate(String.Format(
+                "https://oauth.vk.com/authorize?client_id={0}&redirect_uri={1}&display=page&scope={2}&response_type=token&v=5.53",
+                        VkData.clientId, VkData.redirectUri, VkData.scope));
+        }
+
+        private void FillSongList()
+        {
+            //получаем xml
+            //парсим
+            //заполняем список
+        }
+    }
+}
+
+class Song
+{
+    public Song(string a, string t)
+    { artist = a; title = t; }
+
+    private string artist;
+    private string title;
+
+    public override string ToString()
+    {
+        return artist + " – " + title;
+    }
+}
